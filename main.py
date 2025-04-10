75957b5f05afb8852212a2a5d7787d3159626278
Lucas <e12435611@student.tuwien.ac.at>
2025-04-10 17:40:32 +0200

    created test setup

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 # import local files here
-# from functions.py import *
+from functions import compute_local_stiffness_matrix
+from mesh import Mesh
+
+testing = True
 
 def main():
-    pass
+    if testing:
+        # Create 2 by 2 mesh
+        mesh = Mesh(4)
+        for node in mesh.nodes:
+            print(f"Node: {node.id}, Coordinates: ({node.x}, {node.y})")
+        for element in mesh.elements:
+            print(f"Element: {element.id}, Nodes: ({element.n1.id}, {element.n2.id}, {element.n3.id})")
+
+    else:
+        # create real mesh
+        pass
+    
+
+
 
 if __name__ == "__main__":
     main()
