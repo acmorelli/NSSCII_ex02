ac0e35b19b989ee33a8ed8b7536ebfe601dcc831
Lucas <e12435611@student.tuwien.ac.at>
2025-04-10 13:01:30 +0200

    Created main.py file for workflow and functions.py for helpful functions (like computing the local stiffness matrix)

---
+++ functions.py
@@ -0,0 +1,4 @@
+def compute_local_stiffness_matrix(triangle):
+    pass
+def compute_local_force_vector(mesh):
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+# import local files here
+# from functions.py import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
